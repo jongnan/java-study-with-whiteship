0f1741c91eaef53ed498b9e14ae9c9dad3679a8f
jong yoon <dhorlawhddbs@naver.com>
2020-12-07 16:27:35 +0900

    [Add] ListNode 구현
    Linked List에 필요한 ListNode 객체 구현
     * 생성자
     * getData
     * getNext
     * setNext

---
+++ week4/assignment_2/src/main/java/ListNode.java
@@ -0,0 +1,20 @@
+public class ListNode {
+    private int data;
+    private ListNode next;
+
+    ListNode(int data) {
+        this.data = data;
+    }
+
+    public int getData() {
+        return this.data;
+    }
+
+    public ListNode getListNode() {
+        return this.next;
+    }
+
+    public void setNext(ListNode nextNode) {
+        this.next = nextNode;
+    }
+}
