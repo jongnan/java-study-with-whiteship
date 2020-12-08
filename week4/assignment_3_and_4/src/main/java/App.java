8944bbd1cdc9c7a16377192bb75b2f3289b07df3
jong yoon <dhorlawhddbs@naver.com>
2020-12-08 16:53:48 +0900

    [Add] 배열을 사용한 Stack 구현
    정수 타입의 배열을 사용하여 Stack 구현 완료
      * push
      * pop

---
+++ week4/assignment_3_and_4/src/main/java/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) {
+        ArrayStack stack = new ArrayStack(20);
+
+    }
+}
---
+++ week4/assignment_3_and_4/src/main/java/ArrayStack.java
@@ -0,0 +1,43 @@
+import java.util.Stack;
+
+public class ArrayStack {
+    private final int[] store;
+    private final int maxSize;
+    private int size;
+
+    ArrayStack() {
+        final int DEFAULT_SIZE = 10;
+        this.store = new int[DEFAULT_SIZE];
+        this.maxSize = DEFAULT_SIZE;
+        this.size = 0;
+    }
+
+    ArrayStack(int maxSize) {
+        this.store = new int[maxSize];
+        this.maxSize = maxSize;
+        this.size = 0;
+    }
+
+    public void push(int data) {
+        if (isFull()) {
+            throw new RuntimeException("Stack is full...");
+        }
+        this.store[this.size] = data;
+        this.size++;
+    }
+
+    public int pop() {
+        if(isEmpty()) {
+            throw new RuntimeException("Stack is Empty...");
+        }
+        return this.store[this.size--];
+    }
+
+    private boolean isEmpty() {
+        return size == 0;
+    }
+
+    private boolean isFull() {
+        return maxSize == size;
+    }
+}
