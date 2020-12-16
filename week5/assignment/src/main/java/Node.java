6788a987f3678b9be7c4cf5c2813871afe641634
jong yoon <dhorlawhddbs@naver.com>
2020-12-16 14:31:45 +0900

    [Add] 5주차 Optional 과제 시작
    Node 클래스 정의

---
+++ week5/assignment/src/main/java/Node.java
@@ -0,0 +1,33 @@
+public class Node {
+    private int value;
+    private Node left;
+    private Node right;
+
+    Node(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+}
